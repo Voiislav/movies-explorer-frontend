cfd21f9e0977dbde9a7ad3c7cad512f856f4e6b0
Voiislav <vojmitrovic@gmail.com>
2024-02-12 18:15:25 +0100

    feat: add response to movies api

---
+++ src/utils/MoviesApi.js
@@ -0,0 +1,29 @@
+export class MoviesApi {
+  constructor({ baseUrl, headers }) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  _checkResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  }
+
+  getAllMovies() {
+    return fetch(`${this._baseUrl}`, {
+      headers: this._headers
+    })
+    .then(this._checkResponse)
+  }
+}
+
+const moviesApi = new MoviesApi({
+  baseUrl: 'https://api.nomoreparties.co/beatfilm-movies',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+export default moviesApi;

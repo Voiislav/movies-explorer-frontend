31ef3b6f60fdfe5896135d8c23b29c12a2e6d43b
Voiislav <vojmitrovic@gmail.com>
2024-02-13 16:44:16 +0100

    feat: user login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,52 @@
 import Auth from '../Auth/Auth';
+import * as auth from "../auth.js";
+import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
 
-function Login() {
+function Login({ handleLogin }) {
+  const [formValue, setFormValue] = useState({
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (evt) => {
+    const { name, value } = evt.target;
+    setFormValue({
+      ...formValue,
+      [name]: value,
+    });
+  }
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    if (!formValue.email || !formValue.password) {
+      return;
+    }
+    auth
+      .authorize(formValue.email, formValue.password)
+      .then((data) => {
+        if (data.token) {
+          setFormValue({ email: "", password: "" });
+        }
+        handleLogin();
+        navigate("/", { replace: true });
+      })
+      .catch((err) => {
+        if (err.status === 400) {
+          console.log("400 - не передано одно из полей");
+        }
+        if (err.status === 401) {
+          console.log("401 - пользователь с таким email не найден");
+        }
+      });
+  }
+
   return (
     <Auth
+    handleSubmit={handleSubmit}
+    handleChange={handleChange}
     titleText='Рады видеть!'
     submitButtonText='Войти'
     clarificationText='Еще не зарегистрированы?'

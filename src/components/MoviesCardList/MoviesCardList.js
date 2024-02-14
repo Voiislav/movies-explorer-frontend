90f158c7d41f1e74228e813dac5ce6a53b832c1f
Voiislav <vojmitrovic@gmail.com>
2024-02-14 12:54:16 +0100

    feat: add load more button control

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,4 +1,5 @@
 import { useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import './MoviesCardList.css';
 import MoviesCard from '../MoviesCard/MoviesCard';
 
@@ -6,21 +7,39 @@ function MoviesCardList({ movies }) {
   const location = useLocation();
   const isMoviesRoute = location.pathname === '/movies';
 
+  const [visibleCards, setVisibleCards] = useState(4);
+
+  const handleLoadMore = () => {
+    setVisibleCards(prevVisibleCards => prevVisibleCards + (window.innerWidth <= 480 ? 2 : 4));
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setVisibleCards(4);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <section className='movies'>
       <ul className='movies__list'>
-      {movies.map((movie) => (
-            <MoviesCard
-              key={movie.id}
-              name={movie.nameRU}
-              duration={movie.duration}
-              image={movie.image}
-              trailerLink={movie.trailerLink}
-            />
-          ))}
+        {movies.slice(0, visibleCards).map((movie) => (
+          <MoviesCard
+            key={movie.id}
+            name={movie.nameRU}
+            duration={movie.duration}
+            image={movie.image}
+            trailerLink={movie.trailerLink}
+          />
+        ))}
       </ul>
-      {isMoviesRoute && (
-        <button className='movies__more' type='button' aria-label='Загрузить еще'>Ещё</button>
+      {isMoviesRoute && visibleCards < movies.length && (
+        <button className='movies__more' type='button' aria-label='Загрузить еще' onClick={handleLoadMore}>Ещё</button>
       )}
     </section>
   );

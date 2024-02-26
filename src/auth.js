a779e19da9b5f1a7079cf875788cbf5bf65eceea
Voiislav Mitrovich <voiislav@MacBook-Air-Voiislav.local>
2024-02-26 14:56:45 +0100

    refactor: remove tokens from the console

--- src/auth.js
+++ src/auth.js
@@ -35,7 +35,6 @@ export const authorize = (email, password) => {
       if (data.token) {
         const token = data.token;
         localStorage.setItem("token", token);
-        console.log(localStorage.getItem('token'))
       }
       return data;
     });
@@ -43,7 +42,6 @@ export const authorize = (email, password) => {
 
 export const checkToken = () => {
   const token = localStorage.getItem("token");
-  console.log(token);
   return request(`${BASE_URL}/users/me`, {
     method: "GET",
     headers: {
